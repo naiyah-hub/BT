94d182ab2770afcbff939315cfcba3ed79ee1a89
Nai Lewis <babygirl@Naiyahs-MacBook-Pro-2.local>
2024-04-29 23:38:17 -0400

    updated read me

--- booktrk-vite/src/Books.jsx
+++ booktrk-vite/src/Books.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
+import BookList from "./BookList";
 import request from "superagent";
 
 class Book extends Component{
@@ -15,22 +16,27 @@ class Book extends Component{
         e.preventDefault();
         request
             .get("https://www.googleapis.com/books/v1/volumes")
-            .query({q: this.searchField})
+            .query({q: this.state.searchField })
             .then((data)=>{
-                this.section({books: [...data.body.items]})
+                // console.log(data);
+                this.setState({books: [...data.body.items]})
             })
     }
 
     handleSearch = (e) =>{
+        console.log(e.target.value);
         this.setState({searchField: e.target.value})
     }
+
 
     render(){
         return(
             <div>
-                <SearchBar searchBook={this.searchBook} handleSearch={this.handleSearch}></SearchBar>
+                {/* <SearchBar searchBook={this.searchBook} handleSearch={this.handleSearch}></SearchBar> */}
+                <SearchBar searchBook={this.searchBook} handleSearch={this.handleSearch}/>
+                <BookList books={this.state.books}/>
             </div>
-        )
+        );
     }
 }
 
